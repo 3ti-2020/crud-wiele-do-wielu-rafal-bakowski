5aa396791a5d29fa905b48901b53f386570f3384
Rafal001558 <62039524+Rafal001558@users.noreply.github.com>
2020-10-28 12:22:39 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,9 +1,14 @@
 <?php
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="dom";
+// $servername="localhost";
+// $username="root";
+// $password="";
+// $dbname="dom";
+
+$servername="sql7.freemysqlhosting.net";
+     $username="sql7373159";
+     $password="WwcDU7gMfM";
+     $dbname="sql7373159";
 
 $conn= new mysqli($servername,$username,$password,$dbname);
 $sql="INSERT INTO `lib_autor`(`id_autor`, `name`) VALUES (NULL,'".$_POST['name']."') ;";
