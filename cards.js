e805ed887ef17cdf8bcf2bb9f673160df09e426b
Rafal001558 <62039524+Rafal001558@users.noreply.github.com>
2020-11-03 16:25:32 +0100

    Delete cards.js

--- cards.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelectorAll('.btn');
-const food__img = document.querySelector('.food__img');
-
-btn.forEach(element => {
-   element.addEventListener("click", function(){
-      alert("SOLD");
-   });
-   
-});
