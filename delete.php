a3ebc71037143f1d8bb8d0e877e295829be7430b
Rafal001558 <62039524+Rafal001558@users.noreply.github.com>
2020-11-03 11:15:12 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -5,16 +5,12 @@ $username="sql7373159";
 $password="WwcDU7gMfM";
 $dbname="sql7373159";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-
 $conn= new mysqli($servername,$username,$password,$dbname);
 
 $sql="DELETE FROM `lib_aut_tyt` WHERE `lib_aut_tyt`.`id_autor_tytul` = '".$_POST['ID']."'";
 
-
+echo($sql);
 
 mysqli_query($conn, $sql);
 
-header("Location:indx.php");
+// header("Location:index.php");
